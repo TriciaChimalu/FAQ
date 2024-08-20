86e32ee3afc5e55ac6c74615e5486b02e674ff55
TriciaChimalu <ozobialupatricia@gmail.com>
2024-08-20 07:09:46 +0100

    update

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+const answer = document.getElementsByClassName("answers ")
+
+const minBtn= document.getElementsByClassName("min");
+const maxBtn = document.getElementsByClassName("max")
+
+
+
+
